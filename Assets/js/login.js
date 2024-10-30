7bf3a1b70bfc37ddf90545d789319aecd6d58de6
Ashwani Rana <Ashwanirana756@gmail.com>
2024-10-31 01:48:36 +0530

    Update login.js

--- Assets/js/login.js
+++ Assets/js/login.js
@@ -1,18 +1,11 @@
-document.querySelectorAll('.toggle-form').forEach(item => {
-    item.addEventListener('click', event => {
-        const loginSection = document.querySelector('.form-section.login-active');
-        const signupSection = document.querySelector('.form-section:nth-child(2)');
-        
-        loginSection.classList.toggle('login-active');
-        signupSection.classList.toggle('active');
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-        // If login is active, move signup to view
-        if (loginSection.classList.contains('login-active')) {
-            signupSection.style.transform = 'translateX(100%)';
-            loginSection.style.transform = 'translateX(0)';
-        } else {
-            signupSection.style.transform = 'translateX(0)';
-            loginSection.style.transform = 'translateX(-100%)';
-        }
-    });
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
 });
